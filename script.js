934a1f5cd08b0f32f7097d5c6c0a261c01eed32b
pauloricardoz <trybe.przt@gmail.com>
2024-02-08 15:46:11 -0300

    feat: adicionando função de "drop" Cria a funcionalidade para colocar o elemento como filho de outro elemento

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function allowDrop(ev) {
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
 }
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
